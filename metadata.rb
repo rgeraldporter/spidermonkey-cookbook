8a316ffca6a25205ebdaeda6d98fbcd931f2f583
Rob Porter <rob@weeverapps.com>
2014-12-09 09:58:33 -0500

    Initial commit; added required "name" property to metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name			  "spidermonkey"
+maintainer        "Cloudant"
+maintainer_email  "support@cloudant.com"
+license           "Apache 2.0"
+description       "Configures libmozjs"
+version           "0.1"
+long_description  "Configures libmozjs"
